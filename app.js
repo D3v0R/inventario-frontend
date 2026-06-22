1ae7dbaad49f77a0c8209058f395eedb7fa4cc3a
Diego <diegivergara@gmail.com>
2026-06-22 10:14:42 -0600

    Aquí describe lo que cambiaste, ej: Agregué rutas de CRUD

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const API_URL = "https://inventario-backend1-1.onrender.com/productos";
-const API_URL = 'http://localhost:3000/productos';
-
 // 1. Obtener y mostrar productos
 async function obtenerProductos() {
     const res = await fetch(API_URL);
